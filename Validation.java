4ba023b924437db155746b64c4e0c14417451b79
ankita13-1997 <parhiankita@gmail.com>
2021-02-11 01:17:49 +0530

    Added the code of displaying msg

---
+++ Validation.java
@@ -0,0 +1,19 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validation {
+	
+	public static void main(String[] args) 
+	{
+		  System.out.println("welcome to utilities for registration form");    
+	}
+
+	
+
+	
+	
+
+}
