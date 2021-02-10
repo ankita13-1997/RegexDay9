94ef02358ef0aade1fd72948b421257bbaa16351
ankita13-1997 <parhiankita@gmail.com>
2021-02-11 01:19:31 +0530

    validated only name

---
+++ ValidationName.java
@@ -0,0 +1,46 @@
+package practice;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidationName {
+
+	Scanner sc=new Scanner(System.in);
+	void CheckName(){
+		
+		System.out.println("enter your name");
+		System.out.println("please enter the[1] first letter in captical[2]input of min 5 letter and maximum 10 is accepted");
+		System.out.println();
+		String firstName=sc.next(); 
+		
+		String regex="[A-Z]{1}[a-z]{2,10}$";
+		Pattern p = Pattern.compile(regex);
+		
+		 Matcher m = p.matcher(firstName);
+		 boolean m1=m.matches();
+		 System.out.println(m1);
+		
+		 if (m1==false)
+		 {
+			 
+			 System.out.println("invalid inputs in name please renter in the correct format");
+			 
+		 }
+		 
+		 else
+		 {
+			 System.out.println("your name "+firstName);
+		 }
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new ValidationName().CheckName();
+		
+
+	}
+
+}
